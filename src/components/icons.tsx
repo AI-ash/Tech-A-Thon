dc8487052a14a146e92ac083d446107926f36faf
Ashish Sharma <ashishsharmadevil602@gmail.com>
2025-10-30 16:17:28 +0000

    use this asa icoan

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -4,16 +4,14 @@ export const Logo = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 24 24"
-    fill="none"
+    fill="currentColor"
     stroke="currentColor"
-    strokeWidth="1.5"
+    strokeWidth="0"
     strokeLinecap="round"
     strokeLinejoin="round"
     {...props}
   >
-    <path d="M10 9.5 5.5 14l4.5 4.5" />
-    <path d="M14 18.5 18.5 14 14 9.5" />
-    <path d="m8.5 18.5 7-13" />
+    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1.12 15.12c-.28 0-.5-.22-.5-.5v-2.1l-.8.6c-.18.12-.42.15-.63.06-.2-.08-.34-.28-.34-.5V10.8c0-.28.22-.5.5-.5s.5.22.5.5v3.25l1.62-1.22c.18-.14.42-.16.63-.06.2.08.35.28.35.5v4.85c0 .28-.22.5-.5.5zm4.82-1.95-1.9-2.52c-.15-.2-.17-.45-.06-.67.12-.22.34-.36.58-.36h.88V9.1c0-.28.22-.5.5-.5s.5.22.5.5v3.52h.88c.24 0 .46.14.58.36.1.22.08.48-.06.67l-1.9 2.52c-.15.2-.4.2-.55 0z" />
   </svg>
 );
 
