810576da136dd22490649561f563a6b905c3c3ba
Ashish Sharma <ashishsharmadevil602@gmail.com>
2025-10-30 16:16:25 +0000

    not loader working properly

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@ import { Loader2 } from "lucide-react";
 
 export default function Loading() {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-background/80 backdrop-blur-sm z-50">
+    <div className="flex flex-1 items-center justify-center">
       <Loader2 className="h-16 w-16 animate-spin text-primary" />
     </div>
   );
