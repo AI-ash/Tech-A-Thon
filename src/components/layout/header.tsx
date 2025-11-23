88f1f3fc5f7eff70c8191459254bcfc239f13929
Ashish Sharma <ashishsharmadevil602@gmail.com>
2025-11-23 14:41:46 +0000

    increase it's size abd also just glow it with white or anything that's k

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -15,7 +15,7 @@ export default function Header() {
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 max-w-screen-2xl items-center">
         <Link href="/" className="mr-6 flex items-center space-x-2">
-          <Logo className="h-6 w-6 text-primary" />
+          <Logo className="h-8 w-8 text-primary" />
           <span className="font-bold font-headline">Tech-A-Thon</span>
         </Link>
         <nav className="hidden flex-1 gap-6 text-sm font-medium md:flex">
