b77cdafb4dedf15d911e33d818c9676c57f7d261
Ashish Sharma <ashishsharmadevil602@gmail.com>
2025-11-23 11:51:22 +0000

    html : <div class="loader">   <div class="text"><span>Tech-A-Thon</span>

--- src/components/pre-loader.tsx
+++ src/components/pre-loader.tsx
@@ -1,10 +1,35 @@
-import { Logo } from "@/components/icons";
-
 export default function PreLoader() {
   return (
     <div className="fixed inset-0 z-[100] flex items-center justify-center bg-background">
-      <div className="animate-pulse-and-fade">
-        <Logo className="h-28 w-28 text-primary" />
+      <div className="loader">
+        <div className="text">
+          <span>Tech-A-Thon</span>
+        </div>
+        <div className="text">
+          <span>Tech-A-Thon</span>
+        </div>
+        <div className="text">
+          <span>Tech-A-Thon</span>
+        </div>
+        <div className="text">
+          <span>Tech-A-Thon</span>
+        </div>
+        <div className="text">
+          <span>Tech-A-Thon</span>
+        </div>
+        <div className="text">
+          <span>Tech-A-Thon</span>
+        </div>
+        <div className="text">
+          <span>Tech-A-Thon</span>
+        </div>
+        <div className="text">
+          <span>Tech-A-Thon</span>
+        </div>
+        <div className="text">
+          <span>Tech-A-Thon</span>
+        </div>
+        <div className="line"></div>
       </div>
     </div>
   );
